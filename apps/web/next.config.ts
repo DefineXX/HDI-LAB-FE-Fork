c149d198e44619bb63afa4460b573ba6da3bebcc
DefineXX <keaikim77@gmail.com>
2025-09-23 16:24:59 +0900

    feat(packages/ui): Add @hdi/ui as transpiling packages for shared assets usage

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['@repo/ui'],
+  experimental: {
+    optimizePackageImports: ['@repo/ui'],
   },
 };
 
